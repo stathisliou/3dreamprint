a596cf900b6c6a97ea96f8a7acfb4a508e031c3d
kkampardi <kkampardi@gmail.com>
2016-06-10 11:21:52 +0300

    Added product catalog views and urls #6

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 
@@ -8,10 +9,14 @@ class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True, unique=True)
 
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.id, self.slug])
+
     class Meta:
         ordering = ('name',)
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+
 
     def __str__(self):
         return self.name
@@ -28,6 +33,9 @@ class Product(models.Model):
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
 
     class Meta:
         ordering = ('name',)
