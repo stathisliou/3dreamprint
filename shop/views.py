aa579e95af70fde9cb4b2b5891411c3e4e45574b
kkampardi <kkampardi@gmail.com>
2016-07-17 09:39:04 +0300

    Add serialized data to queryset

--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Category, Product
+from .models import Category, Product, ProductManager
 from cart.forms import CartAddProductForm
 
 
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
+    products = Product.in_stock.all()
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
